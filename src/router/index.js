e578e83f01f0386cd4bf478189b8ff8e759ef592
wangchao <wangchao@huachunnet.com>
2018-01-04 09:30:58 +0800

    代码提交

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Recommend from 'components/recommend/recommend'
+import Singer from 'components/singer/singer'
+import Rank from 'components/rank/rank'
+import Serach from 'components/search/search'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [{
+    path: '/',
+    redirect: '/recommend'
+  }, {
+    path: '/singer',
+    component: Singer
+  }, {
+    path: '/recommend',
+    component: Recommend
+  }, {
+    path: '/rank',
+    component: Rank
+  }, {
+    path: '/search',
+    component: Serach
+  }]
+})
